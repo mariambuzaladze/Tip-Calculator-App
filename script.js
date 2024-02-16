946a2e8a2699e24e1d9e77921a592366092e8e24
mariambuzaladze <mariambuzaladze10@example.come>
2024-02-16 20:53:06 +0400

    Add variables for bill and people inputs, along with tip percentages in JavaScript file

---
+++ script.js
@@ -0,0 +1,9 @@
+let billInput = document.getElementsByClassName("input")[0];
+let peopleInput = document.getElementsByClassName("input")[1];
+
+let fivePercent = document.getElementsByClassName("percent")[0];
+let tenPercent = document.getElementsByClassName("percent")[1];
+let fifteenPercent = document.getElementsByClassName("percent")[2];
+let twentyPercent = document.getElementsByClassName("percent")[3];
+let twentyFivePercent = document.getElementsByClassName("percent")[4];
+let userPercent = document.getElementsByClassName("tip-input")[0];
