dd3dd926953e1abfa85a83f449a379a9f47700ae
mariambuzaladze <mariambuzaladze10@example.come>
2024-02-17 23:52:31 +0400

    fix JavaScript functionality

--- script.js
+++ script.js
@@ -1,53 +1,31 @@
 let billInput = document.getElementsByClassName("input")[0];
 let peopleInput = document.getElementsByClassName("input")[1];
 
-let fivePercent = document.getElementsByClassName("percent")[0];
-let tenPercent = document.getElementsByClassName("percent")[1];
-let fifteenPercent = document.getElementsByClassName("percent")[2];
-let twentyPercent = document.getElementsByClassName("percent")[3];
-let twentyFivePercent = document.getElementsByClassName("percent")[4];
+let percents = document.getElementsByClassName("percent");
 let userPercent = document.getElementsByClassName("tip-input")[0];
 
 let bill;
 
 billInput.addEventListener("input", function () {
-  bill = billInput.value;
+  bill = Number(billInput.value);
   tipCalculator();
 });
 
 let peopleNum;
 
 peopleInput.addEventListener("input", function () {
-  peopleNum = peopleInput.value;
+  peopleNum = Number(peopleInput.value);
   tipCalculator();
 });
 
-let tip;
+let tip = 0;
 
-fivePercent.addEventListener("click", function () {
-  tip = 5;
-  tipCalculator();
-});
-
-tenPercent.addEventListener("click", function () {
-  tip = 10;
-  tipCalculator();
-});
-
-fifteenPercent.addEventListener("click", function () {
-  tip = 15;
-  tipCalculator();
-});
-
-twentyPercent.addEventListener("click", function () {
-  tip = 20;
-  tipCalculator();
-});
-
-twentyFivePercent.addEventListener("click", function () {
-  tip = 25;
-  tipCalculator();
-});
+for (let i = 0; i < percents.length; i++) {
+  percents[i].addEventListener("click", function () {
+    tip = Number(percents[i].textContent.split("%")[0]);
+    tipCalculator();
+  });
+}
 
 userPercent.addEventListener("input", function () {
   tip = userPercent.value;
@@ -61,7 +39,7 @@ let tipOutput = document.getElementsByClassName("tip-amount")[0];
 let totalOutput = document.getElementsByClassName("total")[0];
 
 function tipCalculator() {
-  if (bill !== undefined && peopleNum !== undefined && tip !== undefined) {
+  if (!isNaN(bill) && !isNaN(peopleNum) && !isNaN(tip)) {
     tipAmount = (bill * tip) / 100;
     total = (bill + tipAmount) / peopleNum;
 
